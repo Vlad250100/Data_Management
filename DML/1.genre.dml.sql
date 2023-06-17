864f0b559f118947348aca4fcf67525a47a77eec
VanyaVel <58736637+VanyaVel@users.noreply.github.com>
2023-06-17 15:19:58 +0300

    Add files via upload

---
+++ DML/1.genre.dml.sql
@@ -0,0 +1,8 @@
+INSERT INTO Genre (name) VALUES ('Rap');
+INSERT INTO Genre (name) VALUES ('Witch House');
+INSERT INTO Genre (name) VALUES ('Rock');
+INSERT INTO Genre (name) VALUES ('Indie Pop');
+INSERT INTO Genre (name) VALUES ('Hip-hop');
+INSERT INTO Genre (name) VALUES ('Indie Rock');
+INSERT INTO Genre (name) VALUES ('Jazz');
+INSERT INTO Genre (name) VALUES ('Pop');
